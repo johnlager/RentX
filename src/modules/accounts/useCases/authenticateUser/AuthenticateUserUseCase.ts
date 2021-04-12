5470cfc39fb3e232fb5166288619fe3f3b88a46a
Joao Vitor <john.vitor.dss@gmail.com>
2021-04-12 16:16:16 -0300

    feat(RefreshToken): add refresh token

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,7 +2,10 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import auth from "@config/auth";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -11,23 +14,36 @@ interface IRequest {
 }
 
 interface IResponse {
+  token: string;
   user: {
     name: string;
     email: string;
   };
-  token: string;
+  refresh_token: string;
 }
 
 @injectable()
 class AuthenticateUserUseCase {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider
   ) {}
 
   async execute({ email, password }: IRequest): Promise<IResponse> {
     // verify user exists
     const user = await this.usersRepository.findByEmail(email);
+
+    const {
+      expires_in_token,
+      secret_refresh_token,
+      secret_token,
+      expires_in_refresh_token,
+      expires_refresh_token_days,
+    } = auth;
 
     if (!user) {
       throw new AppError("Email or password incorrect!");
@@ -41,10 +57,26 @@ class AuthenticateUserUseCase {
     }
     // generate jwt
 
-    const token = sign({}, "f34fa5ab9805c87d7a19050d14486e82", {
+    const token = sign({}, secret_token, {
       subject: user.id,
-      expiresIn: "1d",
+      expiresIn: expires_in_token,
     });
+
+    const refresh_token = sign({ email }, secret_refresh_token, {
+      subject: user.id,
+      expiresIn: expires_in_refresh_token,
+    });
+
+    const refresh_token_expires_date = this.dateProvider.addDays(
+      expires_refresh_token_days
+    );
+
+    await this.usersTokensRepository.create({
+      user_id: user.id,
+      expires_date: refresh_token_expires_date,
+      refresh_token,
+    });
+    // refresh_token: secret_refresh_token,
 
     return {
       token,
@@ -52,6 +84,7 @@ class AuthenticateUserUseCase {
         name: user.name,
         email: user.email,
       },
+      refresh_token,
     };
   }
 }
--- src/shared/infra/http/middleware/ensureAuthenticated.ts
+++ src/shared/infra/http/middleware/ensureAuthenticated.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import auth from "@config/auth";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -22,17 +22,7 @@ async function ensureAuthenticated(
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(
-      token,
-      "f34fa5ab9805c87d7a19050d14486e82"
-    ) as IPayload;
-
-    const usersRepository = new UsersRepository();
-    const user = usersRepository.findById(user_id);
-
-    if (!user) {
-      throw new AppError("User does not exist", 401);
-    }
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
     request.user = {
       id: user_id,
