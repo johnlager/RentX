250ee6758750f27fc4ee88dcb534f99d11bd7850
Joao Vitor <john.vitor.dss@gmail.com>
2021-04-23 15:43:42 -0300

    fix(Database): fix database connection to use in prod

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,23 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-// interface IOptions {
-//   host: string;
-// }
-
-// getConnectionOptions().then((options) => {
-//   const newOptions = options as IOptions;
-//   newOptions.host = "database";
-//   createConnection({
-//     ...options,
-//   });
-// });
-
-export default async (host = "database"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
