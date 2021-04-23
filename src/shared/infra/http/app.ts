9d479b9a4771bd6c35cc9db10635252b6c00302a
Joao Vitor <john.vitor.dss@gmail.com>
2021-04-23 14:47:12 -0300

    feat(UploadConfig and Storage provider): add update to upload config, local storage provider and AWS S3 storage provider

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import "dotenv/config";
 import swaggerUi from "swagger-ui-express";
 
 import { AppError } from "@shared/errors/AppError";
@@ -8,14 +9,19 @@ import createConnection from "@shared/infra/typeorm";
 
 import "@shared/container";
 
+import upload from "@config/upload";
+
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
 createConnection();
 const app = express();
+app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(express.json());
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+
 app.use(router);
 
 app.use(
