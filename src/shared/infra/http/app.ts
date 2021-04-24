6d5f36df37d928d7770b7bc47a6f8ae378c70e1f
Joao Vitor <john.vitor.dss@gmail.com>
2021-04-24 13:53:24 -0300

    feat(cors): add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,15 +1,14 @@
 import "reflect-metadata";
+import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import "dotenv/config";
 import swaggerUi from "swagger-ui-express";
 
+import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
-
 import "@shared/container";
-
-import upload from "@config/upload";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
@@ -22,6 +21,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
