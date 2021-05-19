de692c59164e219f0692e6f2e3891ceaff3712c0
jv <john.vitor.dss@gmail.com>
2021-05-19 18:49:33 -0300

    fix: sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -43,6 +43,19 @@ app.use(cors());
 app.use(router);
 
 app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
+
+app.use(
   Sentry.Handlers.errorHandler({
     shouldHandleError(error) {
       if (
@@ -55,19 +68,6 @@ app.use(
       return false;
     },
   })
-);
-
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ message: err.message });
-    }
-
-    return response.status(500).json({
-      status: "error",
-      message: `Internal server error - ${err.message}`,
-    });
-  }
 );
 
 export { app };
